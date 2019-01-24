cbf088f83ccccc2b8b4632855b3a943145fdabe6
AvdeevDanil17206 <42992343+AvdeevDanil17206@users.noreply.github.com>
2019-01-24 16:11:14 +0700

    Update Factory.cpp

--- Calculator/Factory.cpp
+++ Calculator/Factory.cpp
@@ -3,16 +3,16 @@
 #include "OperCreator.h"
 #include "Exceptions.h"
 
-Factory* Factory::get_instance() {
+Factory* Factory::GetInstance() {
 	static Factory factory;
 	return &factory;
 }
 
-void Factory::make_instruction(std::string key, OperCreator* value) {
+void Factory::MakeInstruction(std::string key, OperCreator* value) {
 	factory_map[key] = value;
 }
 
-Operator* Factory::get_operator(std::string name) {
+Operator* Factory::GetOperator(std::string name) {
 	if (factory_map.find(name) == factory_map.end())
 		throw negative_num();
 	return factory_map.at(name)->create();
