ff1f95d2c227ec8c40306ee21fe8dfe5be0df75c
AvdeevDanil17206 <42992343+AvdeevDanil17206@users.noreply.github.com>
2019-01-24 16:22:49 +0700

    Update Operator.h

--- Calculator/Operator.h
+++ Calculator/Operator.h
@@ -6,48 +6,48 @@
 
 class Operator {
 public:
-	virtual void make_operation(Meaning&) = 0;
+	virtual void MakeOperation(Meaning&) = 0;
 	virtual ~Operator() = 0;
 };
 
 class Add : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Add() override {};
 };
 
 class Substract : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Substract() override {};
 };
 
 class Multiplicate : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Multiplicate() override {};
 };
 
 class Division : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Division() override {};
 };
 class Sqrt : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Sqrt() override {};
 };
 
 class Pop : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Pop() override {};
 };
 
 class Print : public Operator {
 public:
-	void make_operation(Meaning&) override;
+	void MakeOperation(Meaning&) override;
 	~Print() override {};
 };
 
